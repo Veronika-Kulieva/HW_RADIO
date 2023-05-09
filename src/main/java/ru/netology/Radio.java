142a2742554f3b3278c186da2714a5525b847ec9
Nika <gviliya2012@mail.ru>
2023-05-09 23:04:28 +0300

    Radio.java with lombok

--- src/main/java/ru/netology/Radio.java
+++ src/main/java/ru/netology/Radio.java
@@ -1,52 +1,19 @@
 package ru.netology;
 
-public class Radio {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Radio {
     private int minRadioStation = 0;
     private int maxRadioStation = 9;
     private int currentRadiostation = minRadioStation;
     private int minVolume = 0;
     private int maxVolume = 100;
     private int currentVolume = minVolume;
-
-    public Radio(int size) {
-        maxRadioStation = minRadioStation + (size - 1);
-    }
-
-    public Radio() {
-        currentRadiostation = maxRadioStation;
-        currentVolume = maxVolume;
-    }
-
-    public int getMaxRadioStation() {
-        return maxRadioStation;
-    }
-
-    public int getMinRadioStation() {
-        return minRadioStation;
-    }
-
-    public int getMinVolume() {
-        return minVolume;
-    }
-
-    public int getMaxVolume() {
-        return maxVolume;
-    }
-
-    public int getCurrentRadiostation() {
-        return currentRadiostation;
-    }
-
-    public void setCurrentRadiostation(int newCurrentRadiostation) {
-        if (newCurrentRadiostation < 0) {
-            return;
-        }
-        if (newCurrentRadiostation > maxRadioStation) {
-            return;
-        }
-        currentRadiostation = newCurrentRadiostation;
-    }
 
     public void nextRadiostation() {
         currentRadiostation = currentRadiostation >= maxRadioStation ? 0 : ++currentRadiostation;
@@ -58,27 +25,13 @@ public class Radio {
         setCurrentRadiostation(currentRadiostation);
     }
 
-    public int getCurrentVolume() {
-        return currentVolume;
-    }
-
-    public void setCurrentVolume(int newCurrentVolume) {
-        if (newCurrentVolume < 0) {
-            return;
-        }
-        if (newCurrentVolume > maxVolume) {
-            return;
-        }
-        currentVolume = newCurrentVolume;
-    }
-
     public void increaseVolume() {
-        currentVolume = currentVolume >= maxVolume ? 0 : ++currentVolume;
+        currentVolume = currentVolume >= maxVolume ? maxVolume : ++currentVolume;
         setCurrentVolume(currentVolume);
     }
 
     public void decreaseVolume() {
-        currentVolume = currentVolume >= maxVolume ? 0 : --currentVolume;
+        currentVolume = currentVolume > maxVolume ? 0 : --currentVolume;
         setCurrentVolume(currentVolume);
     }
 }
